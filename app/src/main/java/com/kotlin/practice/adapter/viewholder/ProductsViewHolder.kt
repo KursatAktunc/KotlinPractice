57654f7916517b82b122369b1bf3e9b7c644f641
KursatAktunc <aktunc4498@gmail.com>
2022-05-12 00:33:07 +0300

    Products item design update

--- app/src/main/java/com/kotlin/practice/adapter/viewholder/ProductsViewHolder.kt
+++ app/src/main/java/com/kotlin/practice/adapter/viewholder/ProductsViewHolder.kt
@@ -1,6 +1,8 @@
 package com.kotlin.practice.adapter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.kotlin.practice.R
 import com.kotlin.practice.databinding.ProductsItemBinding
 import com.kotlin.practice.models.Products
 
@@ -8,5 +10,10 @@ class ProductsViewHolder(private val binding: ProductsItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun bind(item: Products) {
         binding.model = item
+        binding.productImageView.load(item.image) {
+            crossfade(true)
+            placeholder(R.mipmap.ic_launcher)
+        }
+
     }
 }
