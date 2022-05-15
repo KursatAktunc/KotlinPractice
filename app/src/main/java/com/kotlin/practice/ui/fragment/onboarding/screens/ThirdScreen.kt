834250fbd0700210fea0c435145a9734b146ddb6
KursatAktunc <aktunc4498@gmail.com>
2022-05-15 18:29:25 +0300

    add onBoard finis

--- app/src/main/java/com/kotlin/practice/ui/fragment/onboarding/screens/ThirdScreen.kt
+++ app/src/main/java/com/kotlin/practice/ui/fragment/onboarding/screens/ThirdScreen.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kotlin.practice.R
 import com.kotlin.practice.databinding.FragmentThirdScreenBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,6 +22,9 @@ class ThirdScreen : Fragment() {
     ): View {
         _binding = FragmentThirdScreenBinding.inflate(inflater, container, false)
 
+        binding.btnFinis.setOnClickListener {
+            findNavController().navigate(R.id.action_onBoardingMainFragment_to_mainFragment)
+        }
 
         return binding.root
     }
