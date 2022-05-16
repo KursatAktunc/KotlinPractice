d910c8edbdec6275bcb0d006db38ee684d90795b
KursatAktunc <aktunc4498@gmail.com>
2022-05-16 13:45:44 +0300

    MutableLiveData changed with MutableStateFlow.

--- app/src/main/java/com/kotlin/practice/ui/fragment/main/screens/ProductsFragment.kt
+++ app/src/main/java/com/kotlin/practice/ui/fragment/main/screens/ProductsFragment.kt
@@ -3,6 +3,7 @@ package com.kotlin.practice.ui.fragment.main.screens
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.kotlin.practice.R
 import com.kotlin.practice.adapter.ProductsAdapter
 import com.kotlin.practice.base.BaseFragment
@@ -11,6 +12,7 @@ import com.kotlin.practice.databinding.FragmentProductsBinding
 import com.kotlin.practice.util.ToastHelper
 import com.kotlin.practice.viewmodel.ProductsFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 private const val TAG: String = "ProductsFragment"
@@ -31,6 +33,31 @@ class ProductsFragment : BaseFragment<FragmentProductsBinding, ProductsFragmentV
         mBinding.viewModel = mViewModel
 
         mViewModel.getAllProducts()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            mViewModel.resource.collect {
+                when (it.status) {
+                    ResourceStatus.PROGRESS -> {
+                        Log.v(TAG, "progress")
+                        mBinding.productProgressBar.visibility = View.VISIBLE
+                    }
+                    ResourceStatus.SUCCESS -> {
+                        Log.v(TAG, "success")
+                        adapter = ProductsAdapter(it.data!!)
+                        mBinding.productsRV.adapter = adapter
+                        mBinding.productProgressBar.visibility = View.GONE
+                    }
+                    ResourceStatus.ERROR -> {
+                        Log.v(TAG, "error")
+                        toastHelper.showToastLong("Error, connection rechecking!")
+                        mBinding.productProgressBar.visibility = View.GONE
+                        mViewModel.getAllProducts()
+                    }
+                }
+            }
+        } //MutableStateFlow ile kullanımı
+
+        /*mViewModel.getAllProducts()
         mViewModel.resource.observe(viewLifecycleOwner) {
             when (it.status) {
                 ResourceStatus.PROGRESS -> {
@@ -50,6 +77,6 @@ class ProductsFragment : BaseFragment<FragmentProductsBinding, ProductsFragmentV
                     mViewModel.getAllProducts()
                 }
             }
-        }
+        }*/ //MutableLiveData ile kullanım
     }
 }
--- app/src/main/java/com/kotlin/practice/viewmodel/ProductsFragmentViewModel.kt
+++ app/src/main/java/com/kotlin/practice/viewmodel/ProductsFragmentViewModel.kt
@@ -1,6 +1,5 @@
 package com.kotlin.practice.viewmodel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.kotlin.practice.base.BaseViewModel
 import com.kotlin.practice.base.Resource
@@ -11,37 +10,40 @@ import com.kotlin.practice.repository.ProductsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ProductsFragmentViewModel @Inject constructor(private val repository: ProductsRepository) :
     BaseViewModel() {
-    val resource = MutableLiveData<Resource<List<Products>>>()
+
+    private val _resource =
+        MutableStateFlow<Resource<List<Products>>>(Resource(ResourceStatus.PROGRESS))
+    val resource: StateFlow<Resource<List<Products>>> = _resource
+
+    //val resource = MutableLiveData<Resource<List<Products>>>()
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        resource.postValue(
-            Resource(
-                ResourceStatus.ERROR, null,
-                throwable.localizedMessage?.let { ResourceError(62, it) })
-        )
+        _resource.value = Resource(
+            ResourceStatus.ERROR,
+            null,
+            throwable.localizedMessage?.let { ResourceError(62, it) })
     }
 
     fun getAllProducts() {
-        resource.postValue(Resource(ResourceStatus.PROGRESS))
+        _resource.value = Resource(ResourceStatus.PROGRESS)
 
         viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
             val response = repository.getAllProducts()
+
             if (response.isSuccessful)
-                resource.postValue(Resource(ResourceStatus.SUCCESS, response.body()))
-            else {
-                resource.postValue(
-                    Resource(
-                        ResourceStatus.ERROR, null,
-                        ResourceError(61, response.message())
-                    )
-                )
-            }
+                _resource.value = Resource(ResourceStatus.SUCCESS, response.body())
+            else
+                _resource.value =
+                    Resource(ResourceStatus.ERROR, null, ResourceError(61, response.message()))
+
         }
     }
 }
